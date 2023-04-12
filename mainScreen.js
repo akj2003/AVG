b14f5cb22e6aa9d1c1bf0674069b8676f9882037
Aswin Kumar Jegadeesh <achuj2003@gmail.com>
2023-04-12 12:43:31 -0400

    changes

--- mainScreen.js
+++ mainScreen.js
@@ -11,9 +11,11 @@ var catObject = {
         
     for (var cat in catObject) {
        catSel1.options[catSel1.options.length] = new Option(cat, cat);
+       catSel1.options.value =cat;
     }
     for (var cat in catObject) {
        catSel.options[catSel.options.length] = new Option(cat, cat);
+       catSel.options.value = cat;
     }
     document.getElementById("mod_viewdetails").style.display = "none";
     document.getElementById("mod_Request").style.display = "none";
@@ -21,15 +23,46 @@ var catObject = {
  }
  
  function room() {
+   var roomSel = document.getElementById("roomSel");
     roomSel.length = 1;
     if (this.selectedIndex < 1) return;
     var rooms = catObject[catSel.value];
     for (var i = 0; i < rooms.length; i++) {
        roomSel.options[roomSel.options.length] = new Option(rooms[i], rooms[i])
+       roomSel.options.value = rooms[i];
     }
+    roomsel();
  }
- 
-function mod_viewdetails_enable(value) {
-   document.getElementById("mod_viewdetails").style.display = "block";
-   alert("test");
+
+function roomsel() {
+   var x = document.getElementById("roomSel").value;
+   if(x!='')
+      document.getElementById("mod_viewdetails").style.display = "block";
+   else
+      document.getElementById("mod_viewdetails").style.display = "none";
+      document.getElementById("mod_viewStatus").style.display = "none";
+      document.getElementById("mod_Request").style.display = "none";
+}
+
+function initrequest() {
+   document.getElementById("mod_Request").style.display = "block";
+   document.getElementById("mod_viewStatus").style.display = "none";
+   document.getElementById("home").style.display = "none";
+   document.getElementById("mod_viewdetails").style.display = "none";
+}
+
+function viewstat() {
+   document.getElementById("mod_Request").style.display = "none";
+   document.getElementById("mod_viewStatus").style.display = "block";
+   document.getElementById("home").style.display = "none";
+   document.getElementById("mod_viewdetails").style.display = "none";
+}
+
+function home() {
+   document.getElementById("mod_Request").style.display = "none";
+   document.getElementById("mod_viewStatus").style.display = "none";
+   document.getElementById("home").style.display = "block";
+   document.getElementById("mod_viewdetails").style.display = "none";
+   document.getElementById("roomSel").selectedIndex = 0
+   document.getElementById("catSel").selectedIndex = 0
 }
